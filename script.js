f350234a18405fb2ac81dc6dab1596204883c628
Andrew <andrewpeliza@hotmail.com>
2019-05-25 21:25:50 +0100

    Basic ts types also includes -> tuples, enums etc

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 "use strict";
-console.log('it works!');
+// string
+let myName = 'Andrew';
+// myName = 28;
+// number
+let myAge = 24;
+// myAge = 'andrew';
+//boolean
+let hasHobbies = false;
+// hasHobbies = 1;
+// assign types
+let myRealAge;
+myRealAge = 27;
+// myRealAge = '27';
+// array
+let hobbies = ['cooking', 'sports'];
+// console.log(typeof hobbies);
+hobbies = [100];
+// hobbies = ['A string of strings'];
+// tuples
+let address = ['Superstreet', 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Grey"] = 0] = "Grey";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+})(Color || (Color = {}));
+let myColor = Color.Green;
+// console.log(myColor);
+//any
+let car = 'BMW';
+console.log(car);
+car = { brand: 'BMW', series: 3 };
+console.log(car);
--- script.ts
+++ script.ts
@@ -1,1 +1,42 @@
-console.log('it works!');
+// string
+let myName: string = 'Andrew';
+// myName = 28;
+
+// number
+let myAge: number = 24;
+// myAge = 'andrew';
+
+//boolean
+let hasHobbies: boolean = false;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27';
+
+// array
+let hobbies: any[] = ['cooking', 'sports'];
+// console.log(typeof hobbies);
+hobbies = [100];
+// hobbies = ['A string of strings'];
+
+// tuples
+let address: [string, number] = ['Superstreet', 99];
+
+// enum
+enum Color {
+  Grey,
+  Green = 100,
+  Blue
+}
+
+let myColor: Color = Color.Green;
+// console.log(myColor);
+
+//any
+let car: any = 'BMW';
+console.log(car);
+
+car = { brand: 'BMW', series: 3 };
+console.log(car);
