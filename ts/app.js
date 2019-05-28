db1e76ceb2b9908c6c881a7f04068f40f2955060
Andrew <andrewpeliza@hotmail.com>
2019-05-28 23:25:30 +0100

    Default Params, Rest & Spread.

--- ts/app.js
+++ ts/app.js
@@ -1,5 +1,39 @@
 "use strict";
-let myName = 'Andrew';
-let myAge = 24;
-let anything;
-anything = 12;
+// let myName: string = 'Andrew';
+// let myAge: number = 24;
+// let anything: any;
+// anything = 12;
+// function controlMe(isTrue: boolean) {
+//   let result: number;
+//   if (isTrue) {
+//     result = 12;
+//   }
+//   result = 33;
+//   return result;
+// }
+// let & const
+// let variable = 'Test';
+// console.log(variable);
+// Default parameters
+console.log('Default parameters');
+const countdown = (start = 10) => {
+    while (start > 0) {
+        start--;
+    }
+    console.log('Done!', start);
+};
+countdown(10);
+// Rest & Spread
+console.log('Rest & Spread');
+const numbers = [1, 10, 99, -5];
+// console.log(Math.max(33, 99, 10, -3));
+console.log(Math.max(...numbers));
+// Rest
+function makeArray(...args) {
+    return args;
+}
+console.log(makeArray(1, 2, 6));
+function makeArray2(name, ...args) {
+    return name + args;
+}
+console.log(makeArray2('Andrew ', 1, 2, 6));
--- ts/app.ts
+++ ts/app.ts
@@ -1,13 +1,44 @@
-let myName: string = 'Andrew';
-let myAge: number = 24;
-let anything: any;
-anything = 12;
+// let myName: string = 'Andrew';
+// let myAge: number = 24;
+// let anything: any;
+// anything = 12;
 
-function controlMe(isTrue: boolean) {
-  let result: number;
-  if (isTrue) {
-    result = 12;
+// function controlMe(isTrue: boolean) {
+//   let result: number;
+//   if (isTrue) {
+//     result = 12;
+//   }
+//   result = 33;
+//   return result;
+// }
+
+// let & const
+// let variable = 'Test';
+// console.log(variable);
+
+// Default parameters
+console.log('Default parameters');
+const countdown = (start: number = 10): void => {
+  while (start > 0) {
+    start--;
   }
-  result = 33;
-  return result;
+  console.log('Done!', start);
+};
+countdown(10);
+
+// Rest & Spread
+console.log('Rest & Spread');
+const numbers = [1, 10, 99, -5];
+// console.log(Math.max(33, 99, 10, -3));
+console.log(Math.max(...numbers));
+
+// Rest
+function makeArray(...args: number[]) {
+  return args;
 }
+console.log(makeArray(1, 2, 6));
+
+function makeArray2(name: string, ...args: number[]) {
+  return name + args;
+}
+console.log(makeArray2('Andrew ', 1, 2, 6));
