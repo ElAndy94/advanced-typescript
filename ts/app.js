88df93229ee65a3df159fef2f51c172894707a24
Andrew <andrewpeliza@hotmail.com>
2019-05-29 23:18:22 +0100

    Destructing Arrays /Destructing Objects / Template Literals

--- ts/app.js
+++ ts/app.js
@@ -37,3 +37,23 @@ function makeArray2(name, ...args) {
     return name + args;
 }
 console.log(makeArray2('Andrew ', 1, 2, 6));
+// Destructuring
+console.log('Destructuring');
+const myHobbies = ['cooking', 'sports'];
+// const hobby1 = myHobbies[0];
+// const hobby2 = myHobbies[1];
+const [hobby1, hobby2] = myHobbies;
+console.log(hobby1, hobby2);
+const userData = { userName: 'Andrew', age: 24 };
+// const userName = userData.userName;
+// const age = userData.age;
+const { userName, age } = userData;
+// if you wish to name things differently then do this ->
+const { userName: myName, age: myAge } = userData;
+console.log(myName, myAge);
+// Template Literals
+const myName2 = 'Andrew';
+const greeting = `This is a heading!
+I'm ${myName2}
+This is very basic!`;
+console.log(greeting);
