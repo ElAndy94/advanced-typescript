e620556353307e371c1b806e7751a46fa0428017
Andrew <andrewpeliza@hotmail.com>
2019-06-04 18:04:28 +0100

    Namespace imports

--- ts/namespaces/namespace.js
+++ ts/namespaces/namespace.js
@@ -13,6 +13,8 @@ var MyMath;
     }
     MyMath.calculateRectangle = calculateRectangle;
 })(MyMath || (MyMath = {}));
+/// <reference path="circleMath.ts" />
+/// <reference path="rectangleMath.ts" />
 var PI = 2.99;
 console.log(MyMath.calculateRectangle(10, 20));
 console.log(MyMath.calculateCircumference(3));
--- ts/namespaces/namespace.ts
+++ ts/namespaces/namespace.ts
@@ -1,3 +1,6 @@
+/// <reference path="circleMath.ts" />
+/// <reference path="rectangleMath.ts" />
+
 const PI = 2.99;
 
 console.log(MyMath.calculateRectangle(10, 20));
